d4f798456ecd73aca5b787538a946ad938537ef5
OleksandrDrabyk <sashadrabyk2017@gmail.com>
2025-05-11 13:24:53 +0300

    Оновив компонент програми, щоб відобразити MovieList із верхнім і нижнім колонтитулами

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { movies } from './data/movies';
+import MovieList from './components/MovieList';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.app}>
+      <header className={styles.header}>
+        <h1>Кінотеатр "Кіносвіт"</h1>
       </header>
+      <main className={styles.main}>
+        <MovieList movies={movies} />
+      </main>
+      <footer className={styles.footer}>
+        <p>© 2025 Кінотеатр. Всі права захищені.</p>
+      </footer>
     </div>
   );
 }
