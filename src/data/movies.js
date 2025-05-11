b117b012a165fc5bcc74561bdcf143429802b88c
OleksandrDrabyk <sashadrabyk2017@gmail.com>
2025-05-11 12:34:17 +0300

    Додав дані для 3 фільмів

---
+++ src/data/movies.js
@@ -0,0 +1,41 @@
+export const movies = [
+    {
+      id: 1,
+      title: "Форсаж 10",
+      description: "Доміник Торетто і його родина зіткнулися з найнебезпечнішим ворогом, якого вони коли-небудь зустрічали.",
+      genre: "Екшн",
+      duration: 141,
+      posterUrl: "https://www.themoviedb.org/t/p/original/fiVW06jE7z9YnO4trhaMEdclSiC.jpg",
+      sessions: [
+        { date: "2025-06-15", time: "10:00", hall: "Зал 1" },
+        { date: "2025-06-15", time: "14:30", hall: "Зал 3" },
+        { date: "2025-06-16", time: "19:00", hall: "Зал 2" }
+      ]
+    },
+    {
+      id: 2,
+      title: "Аватар: Шлях води",
+      description: "Джейк Саллі та його родина борються за виживання на Пандорі, тікаючи від нової загрози.",
+      genre: "Фантастика",
+      duration: 192,
+      posterUrl: "https://www.themoviedb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg",
+      sessions: [
+        { date: "2025-06-16", time: "11:30", hall: "Зал 2" },
+        { date: "2025-06-17", time: "16:00", hall: "Зал 1" },
+        { date: "2025-06-17", time: "20:30", hall: "Зал 3" }
+      ]
+    },
+    {
+      id: 3,
+      title: "Вартові галактики 3",
+      description: "Команда Вартових галактики вирушає у небезпечну місію, щоб врятувати одного зі своїх.",
+      genre: "Фантастика",
+      duration: 150,
+      posterUrl: "https://www.themoviedb.org/t/p/original/r2J02Z2OpNTctfOSN1Ydgii51I3.jpg",
+      sessions: [
+        { date: "2025-06-17", time: "12:00", hall: "Зал 3" },
+        { date: "2025-06-18", time: "15:30", hall: "Зал 2" },
+        { date: "2025-06-18", time: "21:00", hall: "Зал 1" }
+      ]
+    }
+  ];
